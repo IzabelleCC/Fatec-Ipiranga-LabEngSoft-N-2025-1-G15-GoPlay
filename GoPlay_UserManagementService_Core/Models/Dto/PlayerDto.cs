8ea9a4da1687a75067cdbdf12d8ed6cdce91594c
Izabelle Cesario <izabellecesario@hotmail.com>
2025-03-09 12:40:19 -0300

    GOP -13 Cria entities e Dto Player e TournamentAdmin

---
+++ GoPlay_UserManagementService_Core/Models/Dto/PlayerDto.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GoPlay_UserManagementService_Core.Entities;
+
+namespace GoPlay_UserManagementService_Core.Models.Dto
+{
+    public abstract class PlayerDto : UserDto
+    {
+        public override int IdUser { get; set; }
+        public override string Name { get; set; }
+        public override string Email { get; set; }
+        public override string Login { get; set; }
+        public override string Password { get; set; }
+        public override string? InstagramPage { get; set; }
+        public override int UserTypeId { get; set; }
+        public string Cpf { get; set; }
+        public string Gender { get; set; }
+        public DateOnly BirthDate { get; set; }
+        public string? TShirtSize { get; set; }
+
+        protected PlayerDto()
+        {
+            
+        }
+
+        public PlayerDto(UserEntity entity) : base(entity)
+        {
+            IdUser = entity.IdUser;
+            Name = entity.Name;
+            Email = entity.Email;
+            Login = entity.Login;
+            Password = entity.Password;
+            InstagramPage = entity.InstagramPage;
+            UserTypeId = entity.UserTypeId;
+            UserType = entity.UserType;
+            Player = entity.Player;
+            TournamentAdmin = entity.TournamentAdmin;
+            
+            if(entity.Player != null)
+            {
+                Cpf = entity.Player.Cpf;
+                Gender = entity.Player.Gender;
+                BirthDate = entity.Player.BirthDate;
+                TShirtSize = entity.Player.TShirtSize;
+            }
+
+        }
+    }
+}
---
+++ GoPlay_UserManagementService_Core/Models/Dto/UserDto.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GoPlay_UserManagementService_Core.Entities;
+
+namespace GoPlay_UserManagementService_Core.Models.Dto
+{
+    public abstract class UserDto
+    {
+        public virtual int IdUser { get; set; }
+        public virtual string Name { get; set; }
+        public virtual string Email { get; set; }
+        public virtual string Login { get; set; }
+        public virtual string Password { get; set; }
+        public virtual string? InstagramPage { get; set; }
+        public virtual int UserTypeId { get; set; }
+        public virtual UserTypeEntity UserType { get; set; }
+        public virtual PlayerEntity? Player { get; set; }
+        public virtual TournamentAdmin? TournamentAdmin { get; set; }
+
+        protected UserDto()
+        {
+            
+        }
+        protected UserDto(UserEntity entity)
+        {
+            IdUser = entity.IdUser;
+            Name = entity.Name;
+            Email = entity.Email;
+            Login = entity.Login;
+            Password = entity.Password;
+            InstagramPage = entity.InstagramPage;
+            UserTypeId = entity.UserTypeId;
+            UserType = entity.UserType;
+            Player = entity.Player;
+            TournamentAdmin = entity.TournamentAdmin;
+        }
+    }
+}
