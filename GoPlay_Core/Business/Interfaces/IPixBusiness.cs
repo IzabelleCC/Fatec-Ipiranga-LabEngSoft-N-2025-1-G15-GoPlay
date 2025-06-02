4abb3a60010539bed1b97d558bf7853026093d9e
Izabelle Cesario <izabellecesario@hotmail.com>
2025-06-01 21:29:47 -0300

    GOP-105 Adiciona metodo na interface

--- GoPlay_Core/Business/Interfaces/IPixBusiness.cs
+++ GoPlay_Core/Business/Interfaces/IPixBusiness.cs
@@ -7,6 +7,6 @@ namespace GoPlay_Core.Business.Interfaces
         Task<string> GeneratePixForRegistration(int registrationId, string userId, CancellationToken cancellationToken);
         Task ConfirmPaymentByTxIdAsync(string txid, string userId, CancellationToken cancellationToken);
         Task<PixRequestData> CreatePixRequestData(int registrationId, string userId, CancellationToken cancellationToken);
-
+        Task RegisterWebhookAsync(string chavePix, string webhookUrl, CancellationToken cancellationToken);
     }
 }
