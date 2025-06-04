f666b879bcb7b97ecfbe7b576391dd4dec93be44
Izabelle Cesario <izabellecesario@hotmail.com>
2025-06-04 01:23:40 -0300

    GOP-142 Adiciona GameMatch

---
+++ GoPlay_Core/Entities/GameMatchEntity.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using GoPlay_Core.Enum;
+
+namespace GoPlay_Core.Entities
+{
+    public class GameMatchEntity
+    {
+        public int Id { get; set; }
+
+        public int GroupNumber { get; set; }
+
+        public int Competitor1Id { get; set; }
+
+        public int Competitor2Id { get; set; }
+
+        public MatchStageEnum MatchStage { get; set; }
+        public DateTime? MatchTime { get; set; }
+        public int? CourtNumber { get; set; }
+
+        public int QtdGames1 { get; set; }
+
+        public int QtdGames2 { get; set; }
+        public int Result { get; set; }
+
+        public CategoryPlayerEntity Competitor1 { get; set; } = null!;
+
+        public CategoryPlayerEntity Competitor2 { get; set; } = null!;
+    }
+}
